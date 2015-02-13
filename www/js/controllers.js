ce58a0e9af6318ae774b5ceffbee82a4fb965193
Mauro Bianchi <bianchimro@gmail.com>
2015-02-13 16:38:57 +0100

    closes #1

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -16,12 +16,25 @@ angular.module("AirBook")
     console.log("Books")
 
     $scope.books = [];
+    $scope.filters = {
+        min_price : null,
+        max_price : null,
+        title_contains :null
+    };
     var updating = false;
 
     var updateFromServer = function(page){
         updating = true;
-        Restangular.all('books').getList({page:page})
+        var params = angular.copy($scope.filters);
+        params.page = page;
+
+        if(page == 1){
+            $scope.books = [];
+        }
+
+        Restangular.all('books').getList(params)
         .then(function(data){
+            console.log("response", data.metadata.count);
             $scope.books = $scope.books.concat(data);
             $scope.metadata = data.metadata;
             updating = false;
@@ -29,12 +42,27 @@ angular.module("AirBook")
     };
 
     $scope.updateBooks = function(){
-        console.log("ss")
         if(updating){return;}
         if($scope.metadata && $scope.metadata.next){
             updateFromServer($scope.metadata.number + 1);
         }
     };
+
+    
+    
+    $scope.search = function(){
+        console.log("filters", $scope.filters);
+        updateFromServer(1);
+    }
+    
+
+    $scope.$watch('filters', function(nv, ov){
+        if(angular.equals(nv, ov)){
+            return;
+        }
+        $scope.search();
+
+    }, true)
 
     updateFromServer(1);
 
