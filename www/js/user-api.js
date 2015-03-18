5ba00eaf4a5983b7d45f70ebe74fd271357459d9
Mauro Bianchi <bianchimro@gmail.com>
2015-03-18 13:01:24 +0100

    added save to cart

--- www/js/user-api.js
+++ www/js/user-api.js
@@ -14,6 +14,18 @@ angular.module("AirBook")
     svc.dropWish = function(bookId){
         return Restangular.one('users/wishes', bookId).remove();
     };
+
+    svc.getCart = function(){
+        return Restangular.all('users/cart').getList();
+    };
+
+    svc.addCart = function(bookId){
+        return Restangular.all('users/cart').post({book:bookId});
+    };
+
+    svc.dropCart = function(bookId){
+        return Restangular.one('users/cart', bookId).remove();
+    };
     
     return svc;
 
